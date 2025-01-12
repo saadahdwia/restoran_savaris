a3cf667acd4c1a9396e13bc99605897ebe698650
faris <fariszakaria03@gmail.com>
2025-01-12 18:28:45 +0700

    merapikan file by:faris

---
+++ example-app/resources/views/pages/about.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css"
+  </head>
+  <body>
+
+  <?php include ('./n&f/navbar.php'); ?>
+
+    <!-- about section -->
+    <br>
+    <br>
+    <section class="about_section layout_padding">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5 col-md-6">
+              <div class="detail-box">
+                <br>
+                <br>
+                <h2>
+                  About us
+                </h2>
+                <p>
+                  Savaris Resto adalah Restoran makanan dan minuman yang berada di daerah Surabaya. nama savaris ini diambil dari Saadah, ivan, dan juga faris. mereka bertiga adalah owner dari resto ini. resto ini menawarkan berbagai macam makanan dan minuman seperti chicken cordon bleu, nasi goreng teri medan, kwetiau siram ayam, nasi ayam goreng mentega dan masih banyak menu lainnya. Solaria restoran berusaha memberikan tempat yang nyaman untuk berkumpul bagi para customernya karena solaria, Your Culinary Journey.
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-7 col-md-6">
+              <div class="img-box">
+                <img src="image/about-img.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <!-- end about section -->
+    
+      <?php include ('./n&f/footer.php'); ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ example-app/resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-white shadow py-3">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="./image/Logo 1.png" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+
+              <li class="nav-item">
+                <a class="nav-link" href="../restoran_savaris/index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../restoran_savaris/about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../restoran_savaris/menu.php">Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../restoran_savaris/Reservasi.php">Reservasi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../restoran_savaris/contact.php">contact</a>
+              </li>
+            </ul>
+            <a href="#" class="btn btn-brand">Order Online</a>
+            <button type="button" class="btn btn-brand">
+             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+              </svg>
+            </button>
+            
+          </div>
+        </div>
+    </nav>
